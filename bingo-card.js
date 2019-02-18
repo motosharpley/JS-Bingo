77d5486344ba7dcaa2e5d7c55eaeda579f7a274e
Bryan <motosharpley@gmail.com>
2019-02-18 15:55:43 -0600

    refactor reload to newcard extract ball draw

--- bingo-card.js
+++ bingo-card.js
@@ -5,7 +5,7 @@ let usedCardNums = new Array(76);
 
 function initAll() {
   if (document.getElementById) {
-    document.getElementById("reload").onclick = anotherCard;
+    document.getElementById("newCard").onclick = anotherCard;
     newCard();
   }
   if (document.getElementById) {
@@ -47,44 +47,5 @@ function anotherCard() {
   };
 
   newCard();
-  return false;
-}
-
-/*
-* Start Ball Draw scripts
-*/
-
-let usedBallNums = new Array(76);
-
-function ballDraw() {
-  for(var i=0 ; i<25 ; i++){
-      setBall(i);
-    }
-}
-
-function setBall(thisBall){
-  let currentBall = "ball" + thisBall;
-  let colPlace = new Array(0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4);
-  let colBasis = colPlace[thisBall] * 15;
-  let newNum = colBasis + getNewNum() + 1;
-
-  do{
-    newNum = colBasis + getNewNum() + 1;
-  }while(usedBallNums[newNum]);
-  
-  usedBallNums[newNum] = true;
-  document.getElementById(currentBall).innerHTML = newNum;
-}
-
-function getNewNum() {
-  return Math.floor(Math.random() * 15);
-}
-
-function anotherBallDraw() {
-  for (var i = 1; i < usedBallNums.length; i++) {
-    usedBallNums[i] = false;
-  };
-
-  ballDraw();
   return false;
 }
