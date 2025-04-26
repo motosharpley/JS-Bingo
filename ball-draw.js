a0f2409b724eda4f4b82be6d72c6f395c297a2e4
Bryan Sharpley <motosharpley@gmail.com>
2025-04-26 09:56:00 -0700

    refactor ball draw display and reset to fix duplicate display at end of game & improve efficiency compare to ball-draw-refactor for exact details

--- ball-draw.js
+++ ball-draw.js
@@ -1,34 +1,98 @@
-let usedBallNums = new Array(76);
+function getNewBall() {
+  return Math.floor(Math.random() * 75 + 1);
+}
 
-function ballDraw() {
-  for(var i=0 ; i<25 ; i++){
-      setBall(i);
+let allBallsDrawn = new Array();
+
+function drawBall() {
+  if (allBallsDrawn.length < 75) {
+    let currentBall;
+    do {
+      currentBall = getNewBall();
+    } while (allBallsDrawn.includes(currentBall));
+
+    allBallsDrawn.push(currentBall);
+
+    // Update the display immediately after drawing a ball
+    displayBallDraw();
+  } else {
+    alert('All Balls Have Been Drawn, Please Start New Game');
+  }
+}
+
+// Draw intial 24 balls to start new game
+let newGameBallDraw = document.getElementById('new-game-ball-draw');
+newGameBallDraw.addEventListener('click', newGameDraw);
+
+function newGameDraw() {
+  if (allBallsDrawn.length < 48) {
+    for (let i = 0; i < 24; i++) {
+      drawBall();
     }
+  } else {
+    alert('Must draw next ball');
+  }
+}
+// end Game init ball draw
+
+let setNewBall = document.getElementById('set-ball');
+setNewBall.addEventListener('click', drawBall);
+
+function displayBallDraw() {
+  let ballsDrawn = document.getElementById('balls-drawn');
+  let li = document.createElement('li');
+
+  // Append only the most recently drawn ball
+  let lastBall = allBallsDrawn[allBallsDrawn.length - 1];
+  li.innerText = lastBall;
+  ballsDrawn.appendChild(li);
+
+  daubCard();
+  setTimeout(() => {
+    interimDaubCard();
+  }, 100);
 }
 
-function setBall(thisBall){
-  let currentBall = "ball" + thisBall;
-  let colPlace = new Array(0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4);
-  let colBasis = colPlace[thisBall] * 15;
-  let newNum = colBasis + getNewNum() + 1;
+// RESET BALL DRAW AND GET FRESH CARD
+let resetBallDraw = document.getElementById('reset-ball');
+resetBallDraw.addEventListener('click', resetBallDisplay);
+resetBallDraw.addEventListener('click', resetBalls);
+resetBallDraw.addEventListener('click', newCard);
 
-  do{
-    newNum = colBasis + getNewNum() + 1;
-  }while(usedBallNums[newNum]);
-  
-  usedBallNums[newNum] = true;
-  document.getElementById(currentBall).innerHTML = newNum;
+function resetBalls() {
+  allBallsDrawn = [];
 }
 
-function getNewNum() {
-  return Math.floor(Math.random() * 15);
+function resetBallDisplay() {
+  let ballDisplay = document.getElementById('balls-drawn');
+  while (ballDisplay.firstChild) {
+    ballDisplay.removeChild(ballDisplay.firstChild);
+  }
+}
+// END RESET GAME
+
+//  AutoPlay
+
+function AutoPlay() {
+  if (allBallsDrawn.length < 75) {
+    drawBall();
+  } else {
+    stopAP();
+  }
 }
 
-function anotherBallDraw() {
-  for (let i = 1; i < usedBallNums.length; i++) {
-    usedBallNums[i] = false;
-  };
+let AP;
 
-  ballDraw();
-  return false;
+function startAP() {
+  AP = setInterval(AutoPlay, 1000);
 }
+
+function stopAP() {
+  clearInterval(AP);
+}
+
+let AutoPlayButton = document.getElementById('auto-play');
+AutoPlayButton.addEventListener('click', startAP);
+
+let stopAutoPlay = document.getElementById('stop-auto-play');
+stopAutoPlay.addEventListener('click', stopAP);
